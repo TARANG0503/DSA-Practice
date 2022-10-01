459c8a3e4ad7f2e0dad5b6045ed465c3ed27c2b1
Vishal Sharma <mrvishalsharma27@gmail.com>
2022-10-01 13:58:57 +0530

    added rat in a maze program in c++

--- LeetCode/final-value-of-variable-after-performing-operations/final-value-of-variable-after-performing-operations.java
+++
@@ -1,9 +0,0 @@
-class Solution {
-    public int finalValueAfterOperations(String[] operations) {
-        int val = 0;
-        for(int i = 0;i<operations.length;i++)
-            if(operations[i].contains("-")) val--;
-            else val++;
-        return val;
-    }
-}
--- LeetCode/remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.java
+++
@@ -1,9 +0,0 @@
-
-class Solution {
-    public String removeOccurrences(String s, String part) {
-        if(!(s.contains(part))){
-           return s;
-        }
-           return removeOccurrences(s.substring(0, s.indexOf(part)) + s.substring(s.indexOf(part)+part.length(),s.length()),part);
-    }
-}
