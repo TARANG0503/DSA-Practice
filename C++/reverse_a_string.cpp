e9edd2ef6c6ba1e8e0e42ea370109305a99e3fe7
shahnok007 <91981968+shahnok007@users.noreply.github.com>
2022-10-05 22:50:40 +0530

    Create reverse_a_string.cpp

---
+++ C++/reverse_a_string.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+          
+void reverseStr(string& str)
+{
+    int n = str.length();
+    for (int i = 0; i < n / 2; i++)
+        swap(str[i], str[n - i - 1]);
+}
